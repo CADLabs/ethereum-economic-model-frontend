3014dde3b1de584d1e1b33ad96641b3d0b1b5294
Vitor Marthendal <marthendalnunes@gmail.com>
2021-06-22 08:39:48 -0300

    add etherscan api key

--- data/api/etherscan.py
+++ data/api/etherscan.py
@@ -1,11 +1,13 @@
 import requests
 import diskcache
+import os
+from dotenv import load_dotenv
 
 from model.types import Wei
 
-
+load_dotenv()
 cache = diskcache.Cache('.etherscan_api.cache')
 
 @cache.memoize(expire=(6 * 60 * 60))  # cached for 6 hours
 def get_eth_supply() -> Wei:
-    return int(requests.get("https://api.etherscan.io/api?module=stats&action=ethsupply", headers={"accept": "application/json"}).json()["result"])
+    return int(requests.get("https://api.etherscan.io/api?module=stats&action=ethsupply", headers={"accept": "application/json", "apikey":os.environ.get('ETHERSCANAPIKEY')}).json()["result"])
