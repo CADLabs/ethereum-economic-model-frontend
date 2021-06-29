291b6c7fbded92d2e58bba197261f698a592abb3
Vitor Marthendal <marthendalnunes@gmail.com>
2021-06-29 11:33:31 -0300

    update flask server

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import dash_auth
 import os
 from dotenv import load_dotenv
-
+import flask
 
 # Plotly custom theme
 import plotly.io as pio
@@ -14,6 +14,9 @@ pio.templates.default = "cadlabs_frontend"
 # Import layout components
 from layout.layout import layout
 
+
+server = flask.Flask(__name__) # define flask app.server
+
 load_dotenv()
 USERNAME = os.environ.get('USERNAME')
 PASSWORD = os.environ.get('PASSWORD')
@@ -21,7 +24,11 @@ VALID_USERNAME_PASSWORD_PAIRS = {
     USERNAME: PASSWORD
 }
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app = dash.Dash(__name__,
+                server=server,
+                suppress_callback_exceptions=True,
+                external_stylesheets=[dbc.themes.LUX])
+    
 app.title = "Eth2 Calculator"
 auth = dash_auth.BasicAuth(
     app,
