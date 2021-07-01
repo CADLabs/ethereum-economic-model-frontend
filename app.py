5de23cfc7fc9fec766d22243177e10ecf2d28ca5
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-01 10:31:02 -0300

    add option for local development withot auth

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 # Dash dependencies
 import dash
 import dash_bootstrap_components as dbc
-import dash_auth
-import os
-from dotenv import load_dotenv
+
 import flask
 
 # Plotly custom theme
@@ -17,12 +15,7 @@ from layout.layout import layout
 
 server = flask.Flask(__name__) # define flask app.server
 
-load_dotenv()
-USERNAME = os.environ.get('USERNAME')
-PASSWORD = os.environ.get('PASSWORD')
-VALID_USERNAME_PASSWORD_PAIRS = {
-    USERNAME: PASSWORD
-}
+
 
 app = dash.Dash(__name__,
                 server=server,
@@ -30,8 +23,4 @@ app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.LUX])
     
 app.title = "Eth2 Calculator"
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
 app.layout = layout
--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-from app import app, server
+import argparse
 
+from app import app, server
+from utils.auth import load_basic_auth
 from layout.eth2_specs.eth2_specs_callbacks import toggle_collapse_eth2
 from layout.validator_specs.validator_specs_callbacks import (toggle_collapse_validator,
                                                               load_validator_specs_defaults,
@@ -8,4 +10,11 @@ from layout.output_graphs.output_graphs_callbacks import run_simulation
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dev', default=False, action='store_true')
+    args = parser.parse_args()
+
+    if not args.dev:
+        load_basic_auth(app)
+
     app.run_server(debug=True)
