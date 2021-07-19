ca0c77b9d39a6de6a46d37b0754e7c8a960c2708
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-19 13:45:45 -0300

    add load simulation data feature

--- app.py
+++ app.py
@@ -9,28 +9,27 @@ import flask
 # Plotly custom theme
 import plotly.io as pio
 from assets import plotly_theme
-pio.templates.default = "cadlabs"
+pio.templates.default = "cadlabs_frontend"
 
 # Import layout components
 from layout.layout import layout
 
 # From notebook
 #from utils import setup
-import sys
-import copy
-import logging
-import numpy as np
 import pandas as pd
-from datetime import datetime
 
 import utils.visualizations as visualizations
-from experiments.run import run
-from model.types import Stage
-from data.historical_values import df_ether_supply
 
 # Import experiment templates
-import experiments.templates.time_domain_analysis as time_domain_analysis
 
+# Import simulation data
+import json
+
+with open('data/simulation_data.json') as json_file:
+    data = json.load(json_file)
+
+simulation_data = data['data']['simulations']
+historical_data = data['data']['historical']
 
 server = flask.Flask(__name__) # define flask app.server
 
@@ -42,44 +41,78 @@ app = dash.Dash(__name__,
 app.title = "Eth2 Calculator"
 app.layout = layout
 
-def run_simulation(validator_adoption, pos_launch_date, eip1559_basefee):
-    # Fetch the time-domain analysis experiment
-    experiment = time_domain_analysis.experiment
-    # Create a copy of the experiment simulation
-    simulation = copy.deepcopy(experiment.simulations[0])
-    simulation_0 = copy.deepcopy(simulation)
-    simulation_0.model.params.update({
-        'validator_process': [
-            lambda _run, _timestep: float(validator_adoption),
-        ],
-        'date_pos': [
-         datetime.strptime(pos_launch_date, "%Y/%m/%d")
-        ],
-        'eip1559_basefee_process': [
-        lambda _run, _timestep: float(eip1559_basefee), 
-    ],  # Gwei per gas
-    })
 
-    df_0, _exceptions = run(simulation_0)
-    return df_0, simulation_0.model.params
+# Util functions
+
+def load_simulation(validator_adoption, pos_launch_date, eip1559_basefee):
+    simulation_data_lookup = (str(pos_launch_date).replace('/', '-').replace('-0', '-') + ':' + 
+                             str(eip1559_basefee) + ':' +
+                             "{:.1f}".format(validator_adoption))
+
+    historical_supply_inflation_pct = historical_data['supply_inflation_pct']
+    historical_eth_supply = historical_data['eth_supply']
+    historical_timestamp = historical_data['timestamp']
+
+    data_historical = {
+        'timestamp': historical_timestamp,
+        'eth_supply': historical_eth_supply,
+        'supply_inflation_pct': historical_supply_inflation_pct,
+        'subset': 0
+    }
+
+    simulation_supply_inflation_pct = simulation_data[simulation_data_lookup]['supply_inflation_pct']
+    simulation_eth_supply = simulation_data[simulation_data_lookup]['eth_supply']
+    simulation_timestamp = simulation_data['timestamp']
+
+    data_simulation = {
+        'timestamp': simulation_timestamp,
+        'eth_supply': simulation_eth_supply,
+        'supply_inflation_pct':simulation_supply_inflation_pct,
+        'subset': 0
+    }
+
+    df_historical_data = pd.DataFrame.from_dict(data_historical)
+    df_historical_data['timestamp'] = pd.to_datetime(df_historical_data['timestamp'])
+     
+
+    df_simulation_data = pd.DataFrame.from_dict(data_simulation)
+    df_simulation_data['timestamp'] = pd.to_datetime(df_simulation_data['timestamp'])
+
+    fig = visualizations.plot_eth_supply_and_inflation_over_all_stages(df_historical_data, df_simulation_data)
+    return fig
+
 
 # Callbacks
-@app.callback(
+
+app.clientside_callback(
+    """
+    function(EIP1559Dropdown) {
+    EIP1559Scenarios = {'Disabled': 0, 'Enabled: Steady State': 90, 'Enabled: MEV':70};
+    if (EIP1559Dropdown === 'Custom'){
+        return window.dash_clientside.no_update
+    }
+    return EIP1559Scenarios[EIP1559Dropdown];
+    }
+    """,
     Output('eip1559-basefee-slider', 'value'),
     [Input('eip1559-dropdown', 'value')]
 )
-def update_eip1559_sliders_by_scenarios(eip1559_dropdown):
-    eip1559_scenarios = {'Disabled': 0, 'Enabled: Steady State': 100, 'Enabled: MEV':70}
-    if eip1559_dropdown == 'Custom':
-        raise PreventUpdate
-    return eip1559_scenarios[eip1559_dropdown]
 
-@app.callback(
+
+app.clientside_callback(
+    """
+    function(ValidatorDropdown) {
+    ValidatorScenarios = {'Normal Adoption': 3, 'Low Adoption': 1.5, 'High Adoption':4.5};
+    if (ValidatorDropdown === 'Custom'){
+        return window.dash_clientside.no_update
+    }
+    return ValidatorScenarios[ValidatorDropdown];
+    }
+    """,
     Output('validator-adoption-slider', 'value'),
     [Input('validator-dropdown', 'value')]
 )
-def update_validator_adoption_sliders_by_scenarios(validator_dropdown):
-    return validator_dropdown
+
 
 
 # Define callback to update graph
@@ -95,28 +128,31 @@ def update_validator_adoption_sliders_by_scenarios(validator_dropdown):
 def update_output_graph(validator_adoption,
                         pos_launch_date,
                         eip1559_basefee):
-    df_0, parameters = run_simulation(
-        validator_adoption,
-        pos_launch_date,
-        eip1559_basefee
-    )
-    if validator_adoption not in [1.5, 3, 4.5]:
-        validator_adoption = 'Custom'
+
+    if validator_adoption == 3:
+        validator_dropdown = 'Normal Adoption'
+    elif validator_adoption == 1.5:
+        validator_dropdown = 'Low Adoption'
+    elif validator_adoption == 4.5:
+        validator_dropdown = 'High Adoption'
+    else:
+        validator_dropdown = 'Custom'
     
-    eip1559_fees = [eip1559_basefee]
-    if eip1559_fees in [0, 100, 70]:
-        if eip1559_fees == 0:
-            eip1559 = 'Disabled'
-        elif eip1559_fees == 100:
-            eip1559 = 'Enabled: Steady State'
-        else:
-            eip1559 = 'Enabled: MEV'
+    eip1559_fees = eip1559_basefee
+    if eip1559_fees == 0:
+        eip1559 = 'Disabled'
+    elif eip1559_fees == 90:
+        eip1559 = 'Enabled: Steady State'
+    elif eip1559_fees == 70:
+        eip1559 = 'Enabled: MEV'
     else:
         eip1559 = 'Custom'
 
-    return (validator_adoption,
+    fig = load_simulation(validator_adoption, pos_launch_date, eip1559_basefee)
+
+    return (validator_dropdown,
             eip1559,
-            visualizations.plot_eth_supply_and_inflation_over_all_stages(df_ether_supply, df_0, parameters=parameters))
+            fig)
 
 if __name__ == '__main__':
     app.run_server(debug=False)
