0af046173a6b48306492eccd73e15140cbf203e9
Vitor Marthendal <marthendalnunes@gmail.com>
2021-08-08 15:51:13 -0300

    change analytics UA

--- app.py
+++ app.py
@@ -89,18 +89,20 @@ app = dash.Dash(__name__,
                     ])
 
 
+app.title = "Ethereum Economic Model"
+app.layout = layout
+
 app.index_string = """<!DOCTYPE html>
 <html>
     <head>
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2XGCY6GEQJ"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-2XGCY6GEQJ');
-            ga('send', 'pageview');
+        <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        
+        ga('create', 'UA-204257094-1', 'auto');
+        ga('send', 'pageview');
         </script>
         {%metas%}
         <title>{%title%}</title>
@@ -117,8 +119,7 @@ app.index_string = """<!DOCTYPE html>
     </body>
 </html>"""
 
-app.title = "Ethereum Economic Model"
-app.layout = layout
+
 
 
 # Callbacks
